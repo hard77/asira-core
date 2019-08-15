c56955332e42f7b3d99cedefa6b0d58232664a1d
Nikolas Wijaya <ayannahnwijaya@ayannah.com>
2019-08-15 17:44:09 +0700

    url

--- src/components/url.js
+++ src/components/url.js
@@ -1,7 +1,6 @@
-
 //  export const serverUrl="https://virtserver.swaggerhub.com/ayannahindonesia/Asira_Lender/1.0.0/"
 //  export const serverUrlBorrower="https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-borrower/"
 
 
- export const serverUrl="http://asira.ayannah.com/api-lender/"
- export const serverUrlBorrower="http://asira.ayannah.com/api-borrower/"
+export const serverUrl="http://asira.ayannah.com/api-lender/"
+export const serverUrlBorrower="http://asira.ayannah.com/api-borrower/"
