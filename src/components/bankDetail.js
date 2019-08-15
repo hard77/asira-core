ac5e03b4c6c689add05b34c6df7d77c948a7d6d1
Nikolas Wijaya <ayannahnwijaya@ayannah.com>
2019-08-15 12:17:17 +0700

    bank detail done

--- src/components/bankDetail.js
+++ src/components/bankDetail.js
@@ -1,80 +1,115 @@
 import React from 'react'
 import Cookies from 'universal-cookie';
 import { Redirect } from 'react-router-dom'
+import {serverUrl} from './url'
+import Moment from 'react-moment'
+
+import axios from 'axios'
+
 const cookie = new Cookies()
 
 class BankDetail extends React.Component{
+    state = {rows:[],layanan:["c","d","s"],produk:["a","b","c"]}
+    componentDidMount(){
+        this.getBankDetail()
+    }
+
+    getBankDetail = ()=>{
+        var id = this.props.match.params.id
+        if(cookie.get('tokenClient')){
+            var config = {
+                headers: {'Authorization': "Bearer " + cookie.get('tokenClient')}
+              };
+          
+            axios.get(serverUrl+`admin/banks/${id}`,config)
+            .then((res)=>{
+                console.log(res.data)
+                this.setState({rows:res.data,layanan:res.data.services,produk:res.data.products})
+            })
+            .catch((err)=>console.log(err))
+        }
+    }
     render(){
         if(cookie.get('token') && cookie.get('tokenClient')){
             return(
                 <div className="container">
-                   <h2>Bank Details</h2>
+                   <h2>Bank Detail</h2>
                    <hr/>
                    
                    <form>
                         <div className="form-group row">
                             <label className="col-sm-4 col-form-label">ID Bank</label>
                             <div className="col-sm-8">
-                            : BABABA
+                            : {this.state.rows.id}
 
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-4 col-form-label">Nama Bank</label>
                             <div className="col-sm-8">
-                            : BABABA
+                            : {this.state.rows.name}
+                            
 
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-4 col-form-label">Tipe Bank</label>
                             <div className="col-sm-8">
-                                : BABABA
+                            : {this.state.rows.type}
+                        
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-4 col-form-label">Alamat Bank</label>
                             <div className="col-sm-8">
-                                 : BABABA
+                            : {this.state.rows.address}
+
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-4 col-form-label">Provinsi</label>
                             <div className="col-sm-8">
-                                 : BABABA
+                            : {this.state.rows.province}
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-4 col-form-label">Kota</label>
                             <div className="col-sm-8">
-                                 : BABABA
+                            : {this.state.rows.city}
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-4 col-form-label">Jenis Layanan</label>
                             <div className="col-sm-8">
-                            : BABABA
+                            : {this.state.layanan === undefined ? "-" :this.state.layanan.toString()}
 
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-4 col-form-label">Jenis Product</label>
                             <div className="col-sm-8">
-                            : BABABA
-
+                            : {this.state.produk === undefined ? "-" :this.state.produk.toString()}
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-4 col-form-label">Nama PIC</label>
                             <div className="col-sm-8">
-                            : BABABA
+                            : {this.state.rows.pic_name}
 
                             </div>
                         </div>
                         <div className="form-group row">
+                            <label className="col-sm-4 col-form-label">No Telp</label>
+                            <div className="col-sm-8">
+                            : {this.state.rows.phone}
+
+                            </div>
+                        </div>
+                        
+                        <div className="form-group row">
                             <label className="col-sm-4 col-form-label">Tanggal Bergabung</label>
                             <div className="col-sm-8">
-                                 : BABABA
+                            :  <Moment date={this.state.rows.join_date} format=" DD  MMMM  YYYY" />                         
 
                             </div>
                         </div>
