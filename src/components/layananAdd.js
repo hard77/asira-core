96f24e2ef5879f952edebd09de1f506c1ec038c6
Nikolas Wijaya <ayannahnwijaya@ayannah.com>
2019-08-16 16:57:34 +0700

    layanan add done

--- src/components/layananAdd.js
+++ src/components/layananAdd.js
@@ -2,13 +2,21 @@ import React from 'react'
 import Cookies from 'universal-cookie';
 import './../support/css/layananAdd.css'
 import { Redirect } from 'react-router-dom'
+import { serverUrl } from './url';
+import swal from 'sweetalert'
+import axios from 'axios'
 const cookie = new Cookies()
 
 class LayananAdd extends React.Component{
     state={
         selectedFile:null,
-        base64img:null
+        base64img:null,
+        errorMessage:'',
+        diKlik:false
     
+    }
+    componentWillReceiveProps(newProps){
+        this.setState({errorMessage:newProps.error})
     }
     
     onChangeHandler = (event)=>{
@@ -33,42 +41,63 @@ class LayananAdd extends React.Component{
      }
 
     btnSimpanLayanan = ()=>{
-      
-        var layanan =this.refs.namaLayanan.value
-        var nilai =  document.querySelector('.messageCheckbox').checked;
-        //var str=''
-        //nilai ? str+="Aktif" : str +="tidak aktif"
-        
-        var pic = this.state.selectedFile
-        var reader = new FileReader();
-        reader.readAsDataURL(pic);
-        reader.onload = function () {
-            alert(nilai)
-            alert(reader.result.replace("data:image/jpeg;base64,",""))
-            alert(layanan)
-           
-        };
-        reader.onerror = function (error) {
-          console.log('Error: ', error);
-        };
-        
-        
+        var name =this.refs.namaLayanan.value
+
+        if(name==="" || this.state.selectedFile===null){
+            this.setState({errorMessage:"Nama Layanan atau Gambar kosong"})
+        }else{
+            var pic = this.state.selectedFile
+            var reader = new FileReader();
+            reader.readAsDataURL(pic);
+            reader.onload =  () => {                
+                var image = reader.result.replace("data:image/jpeg;base64,","")
+                var status =  document.querySelector('.messageCheckbox').checked;
+
+                status ? status= 1: status= 0
+
+                var newData = {name,status,image}
+                var config = {headers: {'Authorization': "Bearer " + cookie.get('tokenClient')}};
+                axios.post(serverUrl+'admin/bank_services',newData,config)
+                .then((res)=>{
+                    swal("Success","Layanan berhasil di tambah","success")
+                    this.setState({errorMessage:null,diKlik:true})
+                })
+                .catch((err)=>{console.log(err)})
+                //masukin axios di sini buat add layanan.. jangan lupa
+
+
+            };
+            reader.onerror = function (error) {
+              console.log('Error: ', error);
+            };
+        }
         
     }
 
-   
-     
+    btnCancel = ()=>{
+        this.setState({diKlik:true})
+    }
 
     render(){
+        if(this.state.diKlik){
+            return <Redirect to='/listlayanan'/>            
+
+        }
         if(cookie.get('token') && cookie.get('tokenClient')){
             return(
                 <div className="container">
                    <h2 className="mt-3">Layanan Tambah</h2>
+                  
                    <hr/>
                    <div className="form-group row">
-                            <label className="col-sm-3">Nama Layanan Bank</label>
+                            <div className="col-12" style={{color:"red",fontSize:"15px",textAlign:'center'}}>
+                                    {this.state.errorMessage}
+                            </div>   
+                    </div>
+                   <div className="form-group row">
+                            <label className="col-sm-3 col-form-label">Nama Layanan</label>
                             <div className="col-sm-9">
-                            <input type="text" placeholder="Masukan Nama Layanan" className="form-control" ref="namaLayanan"></input>                            
+                            <input type="text" placeholder="Masukan Nama Layanan" style={{width:"50%",marginLeft:"100px",height:"35px",borderRadius:"3px"}} ref="namaLayanan"></input>                            
                             </div>
                     </div>
                     <div className="form-group row">
@@ -82,12 +111,12 @@ class LayananAdd extends React.Component{
                             <label className="col-sm-3 col-form-label">Status</label>
                             <div className="col-sm-9">
                             <input className="form-check-input messageCheckbox AddStyleButtonCheckbox" value="active" type="checkbox" /> 
-                            <label style={{position:"relative",left:"145px",paddingTop:"5px"}} >Aktif</label>           
+                            <label style={{position:"relative",left:"130px",paddingTop:"3px"}} >Aktif</label>           
                             </div>
                     </div>
                     <div className="form-group row">
-                            <input type="button" className="btn btn-warning ml-3 mr-3" value="Simpan" onClick={this.btnSimpanLayanan}/>
-                            <input type="button" className="btn btn-secondary" value="Back" onClick={this.btnCancel}/>
+                            <input type="button" className="btn btn-success ml-3 mr-3" value="Simpan" onClick={this.btnSimpanLayanan}/>
+                            <input type="button" className="btn btn-warning" value="Batal" onClick={this.btnCancel}/>
 
                     </div>
                 </div>
