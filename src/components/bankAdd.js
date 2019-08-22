9f86c43d166a2b76d62fdc326a2c08509bef387c
Nikolas Wijaya <ayannahnwijaya@ayannah.com>
2019-08-22 17:36:32 +0700

    add bank fix token PIC variable

--- src/components/bankAdd.js
+++ src/components/bankAdd.js
@@ -169,13 +169,13 @@ class Main extends React.Component{
         var address = this.refs.alamat.value
         var province =  this.refs.provinsi.value.slice(this.refs.provinsi.value.indexOf('T')+1,this.refs.provinsi.value.length)
         var city = this.refs.kota.value
-        var pic_name = this.refs.pic_name.value
+        var pic = this.refs.pic.value
         var phone = this.refs.telp.value
 
         if(this.state.jenisLayanan===null || this.state.jenisProduct===null || 
         this.refs.namaBank.value === "" || this.refs.tipeBank.value ==="0" || 
         this.refs.alamat.value ==="" || this.refs.provinsi.value==="0" || 
-        this.refs.kota.value==="0" || this.refs.pic_name.value ==="" || this.refs.telp.value===""){
+        this.refs.kota.value==="0" || this.refs.pic.value ==="" || this.refs.telp.value===""){
             this.setState({errorMessage:"Harap cek ulang masih ada data yang belum terisi"})
         }else{
             
@@ -187,7 +187,7 @@ class Main extends React.Component{
                     products.push (this.state.jenisProduct[i].value)
                 }
                 var newData = {
-                    name,type,address,province,city,pic_name,phone,services,products
+                    name,type,address,province,city,pic,phone,services,products
                 }
                 var config = {
                     headers: {'Authorization': "Bearer " + cookie.get('token')}
@@ -298,7 +298,7 @@ class Main extends React.Component{
                         <div className="form-group row">
                             <label className="col-sm-2 col-form-label">Nama PIC</label>
                             <div className="col-sm-10">
-                            <input type="text" className="form-control" ref="pic_name" placeholder="Input Nama PIC.." />                            
+                            <input type="text" className="form-control" ref="pic" placeholder="Input Nama PIC.." />                            
                             </div>
                         </div>
                         <div className="form-group row">
