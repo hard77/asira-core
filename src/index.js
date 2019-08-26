ebec92226fdd2aabded8db3e77cb73441bc045e7
ayannahcwinata <51105798+ayannahcwinata@users.noreply.github.com>
2019-08-26 12:31:11 +0700

    Revert "Merge pull request #46 from ayannahindonesia/niko_interface"
    This reverts commit 2a3d5778f30f20a8aae5cb2e6daa93a8fec463d9.

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const globalState = createStore(Reducers,{},applyMiddleware(ReduxThunk))
 ReactDOM.render(
 <Provider store={globalState}>
 
-<BrowserRouter>
+<BrowserRouter basename='/api-core/'>
 <App/>
 </BrowserRouter>
 </Provider>
