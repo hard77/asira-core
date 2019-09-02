02b3d5af6b0c0060345441c3d7d1b3695e573431
Nikolas Wijaya <ayannahnwijaya@ayannah.com>
2019-09-02 16:54:33 +0700

    dropdown value jangka waktu

--- src/components/productAdd.js
+++ src/components/productAdd.js
@@ -79,8 +79,6 @@ class ProductAdd extends React.Component{
             this.setState({errorMessage:"Jangka Waktu Kosong"})
         }else if(parseInt(min_timespan) > parseInt(max_timespan)){
             this.setState({errorMessage:"Jangka Waktu dari lebih besar - Harap cek ulang"})
-        }else if(parseInt(max_timespan) - parseInt(min_timespan) < 6){
-            this.setState({errorMessage:"Jangka Waktu minimal 6 bulan - Harap cek ulang"})
         }
         else if(parseFloat(interest)<0 || parseInt(interest)===0){
             this.setState({errorMessage:"Imbal Hasil tidak bole minus/ kosong - Harap cek ulang"})
@@ -195,58 +193,19 @@ class ProductAdd extends React.Component{
                                 <select ref="jangkaWaktuDari" className="form-control option" style={{width:"150px"}}>
                                         <option value={0}> DARI </option>
                                         <option value={6}>6 </option>
-                                        <option value={7}>7 </option>
-                                        <option value={8}>8 </option>
-                                        <option value={9}>9 </option>
-                                        <option value={10}>10 </option>
-                                        <option value={11}>11 </option>
                                         <option value={12}>12 </option> 
-                                        <option value={13}>13 </option>
-                                        <option value={14}>14 </option>
-                                        <option value={15}>15 </option> 
-                                        <option value={16}>16 </option>
-                                        <option value={17}>17 </option>
                                         <option value={18}>18 </option> 
-                                        <option value={19}>19 </option>
-                                        <option value={20}>20 </option>
-                                        <option value={21}>21 </option>
-                                        <option value={22}>22 </option>
-                                        <option value={23}>23 </option> 
                                         <option value={24}>24 </option>
-                                        <option value={25}>25 </option>
-                                        <option value={26}>26 </option> 
-                                        <option value={27}>27 </option>
-                                        <option value={28}>28 </option>
-                                        <option value={29}>29 </option> 
                                         <option value={30}>30 </option>    
                                 </select>
                               
                                 <select  ref="jangkaWaktuSampai" className="form-control option" style={{width:"150px"}}>
                                         <option value={0}>HINGGA</option>
+                                        <option value={6}>6 </option>
                                         <option value={12}>12 </option> 
-                                        <option value={13}>13 </option>
-                                        <option value={14}>14 </option>
-                                        <option value={15}>15 </option> 
-                                        <option value={16}>16 </option>
-                                        <option value={17}>17 </option>
                                         <option value={18}>18 </option> 
-                                        <option value={19}>19 </option>
-                                        <option value={20}>20 </option>
-                                        <option value={21}>21 </option>
-                                        <option value={22}>22 </option>
-                                        <option value={23}>23 </option> 
                                         <option value={24}>24 </option>
-                                        <option value={25}>25 </option>
-                                        <option value={26}>26 </option> 
-                                        <option value={27}>27 </option>
-                                        <option value={28}>28 </option>
-                                        <option value={29}>29 </option> 
                                         <option value={30}>30 </option>
-                                        <option value={31}>31 </option>
-                                        <option value={32}>32 </option>
-                                        <option value={33}>33 </option>
-                                        <option value={34}>34 </option>
-                                        <option value={35}>35 </option>
                                         <option value={36}>36 </option>
                                 </select>
                                 </td>
