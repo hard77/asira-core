e1dba4ee63badb3efbedb8d0f4be3dbd5afc0c3f
Nikolas Wijaya <ayannahnwijaya@ayannah.com>
2019-09-06 14:39:49 +0700

    empolyee name bug

--- src/components/permintaanPinjamanDetail.js
+++ src/components/permintaanPinjamanDetail.js
@@ -5,11 +5,13 @@ import {serverUrlBorrower} from './url'
 import Moment from 'react-moment';
 
 import Axios from 'axios';
-import swal from 'sweetalert';
 const cookie = new Cookies()
+const config = {
+    headers: {'Authorization': "Bearer " + cookie.get('token')}
+  };
 
 class Main extends React.Component{
-    state = {rows:{},items:[],borrowerDetail:{},status:''}
+    state = {rows:{},items:[],borrowerDetail:{},status:'',borrower_info:{}}
 
     formatMoney=(number)=>
     { return number.toLocaleString('in-RP', {style : 'currency', currency: 'IDR'})}
@@ -23,13 +25,10 @@ class Main extends React.Component{
     getDataDetail =()=>{
        var idLoan = this.props.match.params.idLoan
         if(cookie.get('token')){
-            var config = {
-                headers: {'Authorization': "Bearer " + cookie.get('token')}
-              };
             Axios.get(serverUrlBorrower+`admin/loan/${idLoan}`,config)
             .then((res)=>{
                 console.log(res.data)
-                this.setState({rows:res.data,items:res.data.fees,status:res.data.status})
+                this.setState({rows:res.data,items:res.data.fees,status:res.data.status,borrower_info:res.data.borrower_info})
             })
             .catch((err)=>console.log(err))
         }
@@ -41,9 +40,6 @@ class Main extends React.Component{
     getDataBorrower =()=>{
         var idBorrower = this.props.match.params.idBorrower
         if (cookie.get('token')){
-            var config = {
-                headers: {'Authorization': "Bearer " + cookie.get('token')}
-              };
               Axios.get(serverUrlBorrower+`admin/borrower/${idBorrower}`,config)
               .then((res)=>{
                   console.log(res.data)
@@ -63,9 +59,9 @@ class Main extends React.Component{
 
     getBiayaAdmin =()=>{
         var jsx = this.state.items
-        .map((val)=>{
+        .map((val,index)=>{
             return (
-                    <tr>
+                    <tr key={index}>
                     <td>{val.description}</td>
                     <td>: {this.formatMoney(parseInt(val.amount))}</td>
                     </tr>
@@ -93,7 +89,7 @@ class Main extends React.Component{
                                 </tr>
                                 <tr>
                                     <td>Nama Nasabah</td>
-                                    <td>: {this.state.rows.owner_name}</td>
+                                    <td>: {this.state.borrower_info.employer_name}</td>
                                 </tr>
                                 </tbody>
                              
@@ -224,27 +220,11 @@ class Main extends React.Component{
                                 
                                
                                 </tbody>
-                                
                             </table>
+                            <input type="button" onClick={this.btnBack}  className="mt-2 btn btn-info" value="Back"></input>
                         </div>
             </div>
-             {/* -----------------------------------------------------FOURTH ROW----------------------------------------------------------------- */}
-             <div className="row mt-3">
-                        <div className="col-12 col-md-6">
-                            <table>
-                                <tbody>
-                                    <tr>
-                                 
-                                         <td>
-                                             <input type="button" onClick={this.btnBack}  className="btn btn-info" value="Back"></input>
-                                         </td>
-                                     </tr>
-                               
-                                </tbody>
-                                
-                            </table>
-                        </div>
-            </div>
+             
 
                 </div>
             )
