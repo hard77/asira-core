afa1d794a6a37683c4ccf707818266f21f0dcfa9
Nikolas Wijaya <ayannahnwijaya@ayannah.com>
2019-09-12 14:18:16 +0700

    add new class

--- src/components/header.js
+++ src/components/header.js
@@ -107,6 +107,16 @@ const kukie =new Cookies()
                  <Link to="/listtujuan" style={{color:"inherit",textDecoration:"none"}}>  <DropdownItem>List </DropdownItem>   </Link>                
               </DropdownMenu>
               </UncontrolledDropdown>
+
+              <UncontrolledDropdown  nav inNavbar>
+                <DropdownToggle nav caret style={{ color:"inherit",textDecoration:"none"}}>
+                <label><i class="fas fa-handshake"></i> Role</label>
+                </DropdownToggle>
+              <DropdownMenu className="menuDropDown" style={{border:"1px solid black",marginBottom:"20px"}}>
+                 <Link to="/tambahrole" style={{color:"inherit",textDecoration:"none"}}><DropdownItem>Tambah</DropdownItem></Link>                   
+                 <Link to="/listrole" style={{color:"inherit",textDecoration:"none"}}>  <DropdownItem>List </DropdownItem>   </Link>                
+              </DropdownMenu>
+              </UncontrolledDropdown>
            
         
             <p style={{ cursor:"pointer"}} onClick={this.logOutBtn}><label><i className="fas fa-sign-out-alt"></i> Log Out</label></p>
